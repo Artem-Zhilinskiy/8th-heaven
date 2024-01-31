47d2014a5c52e78177df85c4acc2e14f29e9777d
Artem-Zhilinskiy <Arterm313132@gmail.com>
2024-01-31 20:41:20 +0300

    31.01.2024
    Made a bit of character choice scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EightHeaven
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static byte chosenPC;
+        /*
+         * 0 - Princess;
+         * 1 - Paladin;
+         * 2 - Witch;
+         * 3 - The Cursed One; 
+         */
+
+        void Start()
+        {
+
+        }
+    }
+}
