92d3eba37c14a1cad56b0fbee1b48453c1163ee4
Artem-Zhilinskiy <Arterm313132@gmail.com>
2024-02-05 20:42:23 +0300

    05.02.2024
    Continue to implement main quest

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 namespace EightHeaven
@@ -25,6 +26,60 @@ namespace EightHeaven
         private int actionsPerDay = 3;
         //Day number
         private int dayNumber = 0;
+
+        #region buttons
+        [Header ("Noble quarters buttons")]
+        [SerializeField]
+        private static Button PrincessChambersButton;
+        [SerializeField]
+        private static Button NobleQuartersButton;
+        [SerializeField]
+        private static Button CouncilButton;
+
+        [Header("City buttons")]
+        [SerializeField]
+        private static Button ShopButton;
+        [SerializeField]
+        private static Button SmithButton;
+        [SerializeField]
+        private static Button AlchemistButton;
+        [SerializeField]
+        private static Button LibaryButton;
+        [SerializeField]
+        private static Button BrothelButton;
+        [SerializeField]
+        private static Button TownWalkButton;
+        [SerializeField]
+        private static Button MadamHouseButton;
+
+        [Header("Suburb buttons")]
+        [SerializeField]
+        private static Button FortButton;
+        [SerializeField]
+        private static Button ForestButton;
+        [SerializeField]
+        private static Button FarmsButton;
+        [SerializeField]
+        private static Button CampButton;
+        #endregion
+
+        #region MainQuest
+        private int stage = 0; //Stage of the main quest.
+        private Button[] questLocations = new Button[] { FortButton, ForestButton }; //Location of the current quest stage.
+                                                  //TODO: create massives for questlocations and hints. Element number will be stage number.
+        private string hint; //Hint of the current quest stage.
+
+        public void ProgressStage()
+        {
+            stage++;
+            RenewHint();
+        }
+
+        private void RenewHint()
+        {
+
+        }
+        #endregion
 
         public void NextDay()
         {
